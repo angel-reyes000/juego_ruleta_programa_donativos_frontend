389956f2e0f48de6d4183d3995b5f730dcd907c1
Angel Reyes <ar731684@gmail.com>
2026-09-08 01:35:28 -0600

    updates and creates functions

--- app/configuracion/page.tsx
+++ app/configuracion/page.tsx
@@ -24,13 +24,14 @@ interface Game {
 
 export default function Configuracion () {
     const [gameList, setGameList] = useState<Game[]>();
+    const [prizeList, setPrizeList] = useState<Prize[]>();
     const [game, setGame] = useState<Game>({
         title: "",
         start_datetime: "",
         end_datetime: "",
         max_capacity: 5000,
         description: "",
-        prize_list: [],
+        prize_list: prizeList!,
     });
     const [prize, setPrize] = useState<Prize>({
         name: "",
@@ -40,7 +41,8 @@ export default function Configuracion () {
     })
     const [gameId, setGameId] = useState(0);
     const [inputSearch, setInputSearch] = useState<string>("");
-    const [error, setError] = useState<string>("");
+    const [errorGame, setErrorGame] = useState<string>("");
+    const [errorPrize, setErrorPrize] = useState<string>("");
 
     const router = useRouter();
     const refModalAdd = useRef<any>(null);
@@ -102,11 +104,11 @@ export default function Configuracion () {
             const data = await response.json();
 
             if (response.status !== 200) {
-                setError(data.error)
+                setErrorGame(data.error)
                 return 
             }
 
-            setGameList((prev: any) => [...prev, game])
+            setGameList((prev: any) => [...prev, data])
 
             refModalAdd.current.style.display = 'none';
             refModalAdd.current.close()
@@ -114,7 +116,123 @@ export default function Configuracion () {
         } catch (error) {
             console.log("Error in postGame frontend.")
         } finally {
-            setTimeout(() => setError(""), 5000);
+            setTimeout(() => setErrorGame(""), 5000);
+        }
+    }
+
+    async function updateGame () {
+
+        const token = localStorage.getItem('token');
+
+        try {
+            const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/api/updateGame`, {
+                method: 'PUT',
+                headers: {
+                    'content-type': 'application/json',
+                    'authorization': `Bearer ${token}`,
+                },
+                body: JSON.stringify({
+                    title: game.title,
+                    start_datetime: game.start_datetime,
+                    end_datetime: game.end_datetime,
+                    max_capacity: game.max_capacity,
+                    description: game.description,
+                    prize_list: game.prize_list,
+                    gameId: gameId,
+                })
+            })
+
+            const data = await response.json();
+
+            if (response.status !== 200) {
+                setErrorGame(data.error)
+                return 
+            }
+
+            setGameList(prev => prev?.map(obj => obj.id === data.id ? {
+                    title: data.title,
+                    start_datetime: data.start_datetime,
+                    end_datetime: data.end_datetime,
+                    max_capacity: data.max_capacity,
+                    description: data.description,
+                    prize_list: data.prize_list
+                } : obj))
+
+            refModalEdit.current.style.display = 'none';
+            refModalEdit.current.close();
+
+        } catch (error) {
+            console.log("Error in updateGame frontend.")
+        } finally {
+            setTimeout(() => setErrorGame(""), 5000);
+        }
+    }
+
+    async function getPrizes (gameId: number) {
+
+        const token = localStorage.getItem('token');
+
+        try {
+            const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/api/getPrizes?gameId=${gameId}`, {
+                method: 'GET',
+                headers: {
+                    authorization: `Bearer ${token}`
+                }
+            })
+
+            const data = await response.json();
+
+            if (response.status !== 200) {
+                setErrorPrize(data.error)
+                return 
+            }
+
+            setPrizeList(data)
+
+        } catch (error) {
+            console.log("Error in getPrizes frontend.")
+        } finally {
+            setTimeout(() => setErrorPrize(""), 5000);
+        }
+    }
+
+    async function postPrize () {
+
+        const token = localStorage.getItem('token');
+
+        try {
+            const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/api/postPrize`, {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json',
+                    'authorization': `Bearer ${token}`,
+                },
+                body: JSON.stringify({
+                    name: prize.name,
+                    type: prize.type,
+                    value: prize.value,
+                    round: prize.round,
+                    game_id: gameId, 
+                })
+            })
+
+            const data = await response.json();
+
+            console.log(data)
+            console.log(prize)
+            console.log(gameId)
+
+            if (response.status !== 200) {
+                setErrorPrize(data.error)
+                return 
+            }
+            console.log(prizeList)
+            setPrizeList((prev: any) => [...prev, {id: data.id, name: data.name, type: data.type, round: data.round, value: data.value, game_id: gameId}])
+
+        } catch (error) {
+            console.log("Error in postPrizes frontend.")
+        } finally {
+            setTimeout(() => setErrorPrize(""), 5000);
         }
     }
 
@@ -153,7 +271,7 @@ export default function Configuracion () {
                         <input value={game.description ?? ""} onChange={(e) => setGame(prev => ({...prev, description: e.target.value}))} className='px-2 py-1 font-medium focus:outline-none rounded-md border-2 border-gray-400 focus:border-gray-800'></input>
                     </label>
                 </div>
-                <p className='w-full text-right text-red-500 text-[0.9rem]'>{error}</p>
+                <p className='w-full text-right text-red-500 text-[0.9rem]'>{errorGame}</p>
                 <h1 className='font-bold text-2xl'>Premios</h1>
                 <div className='flex flex-wrap justify-between gap-2'>
                     <label className='flex flex-col font-semibold w-[49%]'>
@@ -171,14 +289,14 @@ export default function Configuracion () {
                     </label>
                     <label className='flex flex-col font-semibold w-[49%]'>
                         Valor$:
-                        <input value={prize.value} onChange={(e) => setPrize(prev => ({...prize, value: Number(e.target.value)}))} type='number' className='px-2 py-1 font-medium focus:outline-none rounded-md border-2 border-gray-400 focus:border-gray-800'></input>
+                        <input value={prize.value} onChange={(e) => setPrize(prev => ({...prev, value: Number(e.target.value)}))} type='number' className='px-2 py-1 font-medium focus:outline-none rounded-md border-2 border-gray-400 focus:border-gray-800'></input>
                     </label>
                     <label className='flex flex-col font-semibold w-[49%]'>
                         ronda:
-                        <input value={prize.round} onChange={(e) => setPrize(prev => ({...prize, round: Number(e.target.value)}))} type='number' min={1} max={5} className='px-2 py-1 font-medium focus:outline-none rounded-md border-2 border-gray-400 focus:border-gray-800'></input>
+                        <input value={prize.round} onChange={(e) => setPrize(prev => ({...prev, round: Number(e.target.value)}))} type='number' min={1} max={5} className='px-2 py-1 font-medium focus:outline-none rounded-md border-2 border-gray-400 focus:border-gray-800'></input>
                     </label>
                     <div className='flex items-end text-white font-semibold'>
-                        <button onClick={() => game.prize_list.length >= 10 ? setError("Maximo 10 premios.") : setGame(prev => ({...prev, prize_list: [...prev.prize_list, {name: prize.name, type: prize.type, value: prize.value, round: prize.round}]}))}
+                        <button onClick={() => game.prize_list.length >= 10 ? setErrorGame("Maximo 10 premios.") : setGame(prev => ({...prev, prize_list: [...prev.prize_list, {name: prize.name, type: prize.type, value: prize.value, round: prize.round}]}))}
                             className='flex items-center bg-blue-500 px-4 py-2 rounded-lg cursor-pointer active:scale-95 gap-1 hover:bg-blue-800'>
                             <FaPlus />Agregar premio
                         </button>
@@ -275,14 +393,15 @@ export default function Configuracion () {
                     </label>
                     <label className='flex flex-col font-semibold w-[49%]'>
                         Valor$:
-                        <input value={prize.value} onChange={(e) => setPrize(prev => ({...prize, value: Number(e.target.value)}))} type='number' className='px-2 py-1 font-medium focus:outline-none rounded-md border-2 border-gray-400 focus:border-gray-800'></input>
+                        <input value={prize.value} onChange={(e) => setPrize(prev => ({...prev, value: Number(e.target.value)}))} type='number' className='px-2 py-1 font-medium focus:outline-none rounded-md border-2 border-gray-400 focus:border-gray-800'></input>
                     </label>
                     <label className='flex flex-col font-semibold w-[49%]'>
                         Ronda:
-                        <input value={prize.round} onChange={(e) => setPrize(prev => ({...prize, round: Number(e.target.value)}))} type='number' min={1} max={5} className='px-2 py-1 font-medium focus:outline-none rounded-md border-2 border-gray-400 focus:border-gray-800'></input>
+                        <input value={prize.round} onChange={(e) => setPrize(prev => ({...prev, round: Number(e.target.value)}))} type='number' min={1} max={5} className='px-2 py-1 font-medium focus:outline-none rounded-md border-2 border-gray-400 focus:border-gray-800'></input>
                     </label>
+                    <p className='w-full text-right text-red-500 text-[0.9rem]'>{errorPrize}</p>
                     <div className='flex items-end text-white font-semibold'>
-                        <button onClick={() => setGame(prev => ({...prev, prize_list: [...prev.prize_list, {name: prize.name, type: prize.type, value: prize.value, round: prize.round}]}))}
+                        <button onClick={() => postPrize()}
                             className='flex items-center bg-blue-500 px-4 py-2 rounded-lg cursor-pointer active:scale-95 gap-1 hover:bg-blue-800'>
                             <FaPlus />Agregar premio
                         </button>
@@ -298,7 +417,7 @@ export default function Configuracion () {
                             </tr>
                         </thead>
                         <tbody>
-                            {game.prize_list?.map(obj => (
+                            {prizeList?.map(obj => (
                                 <tr key={obj.id} className='border-b-1 hover:bg-blue-200'>
                                     <td>{obj.name}</td>
                                     <td>{obj.type}</td>
@@ -315,10 +434,7 @@ export default function Configuracion () {
                         </tbody>
                     </table>
                     <div className='flex justify-between w-full'>
-                        <button onClick={() => {
-                                refModalEdit.current.style.display = 'none';
-                                refModalEdit.current.close()
-                            }}
+                        <button onClick={() => updateGame()}
                             className='flex justify-center items-center cursor-pointer bg-blue-500 text-white p-2 rounded-md active:scale-95 w-[20%] font-semibold gap-1 hover:bg-blue-800'>
                             <FaSave />Guardar
                         </button>
@@ -333,14 +449,14 @@ export default function Configuracion () {
                 </div>
             </dialog>
             {/*-----------------------------------------TABLE OF GAMES---------------------------------------------------*/}
-            <div className="flex flex-col h-dvh p-10 bg-[rgb(30,0,0)] gap-15">
+            <div className="flex flex-col h-auto p-10 bg-[rgb(30,0,0)] gap-15">
                 <div className='flex flex-col text-white gap-2'>
                     <p onClick={() => router.back()} className='cursor-pointer hover:text-blue-400 hover:underline'>{'< Regresar'}</p>
                     <h1 className='text-4xl'>Menu de configuracion</h1>
                 </div>
                 <div className='flex flex-col gap-3'>
                     <div className='flex justify-between text-white w-full'>
-                        <div className='flex items-center px-2 border-3 border-gray-400 rounded-lg gap-2'>
+                        <div className='flex items-center px-2 border-2 border-gray-400 rounded-lg gap-2'>
                             <FaSearch />
                             <input value={inputSearch} onChange={((e) => setInputSearch(e.target.value))} className='focus:outline-none'></input>
                         </div>
@@ -355,6 +471,12 @@ export default function Configuracion () {
                                         max_capacity: 5000,
                                         description: "",
                                         prize_list: [],
+                                })
+                                setPrize({
+                                    name: "",
+                                    type: "Dinero en efectivo",
+                                    value: 0,
+                                    round: 1,
                                 })
                             }}
                             className='flex items-center px-3 py-2 text-md font-semibold rounded-lg cursor-pointer active:scale-95 bg-red-700 gap-1'>
@@ -378,7 +500,15 @@ export default function Configuracion () {
                                 obj.description.includes(inputSearch)
                             )).map((obj: Game) => (
                                 <tr key={obj.id} onClick={() => {
+                                        setGameId(obj.id!);
                                         setGame(obj)
+                                        getPrizes(obj.id!);
+                                        setPrize({
+                                            name: "",
+                                            type: "Dinero en efectivo",
+                                            value: 0,
+                                            round: 1,
+                                        })
                                         refModalEdit.current.showModal()
                                         refModalEdit.current.style.display = 'flex'
                                     }} 
